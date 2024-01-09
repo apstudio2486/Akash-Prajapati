f6a2732dbfe29402e8773d4de738be5f7b36a066
aakashandroid <108667175+aakashandroid@users.noreply.github.com>
2024-01-09 15:29:00 +0530

    update resue path

--- js/main.js
+++ js/main.js
@@ -177,9 +177,13 @@
 
   });
 
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
-
+  $(document).ready(function() {
+  
+    setTimeout(function(){
+      $('body').addClass('loaded');
+      $('h1').css('color','#222222');
+    }, 3000);
+    
+  });
+  
 })()
